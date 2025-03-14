c0b7e004d7cbe1b455bc499e460af492082fd109
Klein <klein.gachuhi@student.moringaschool.com>
2025-03-14 20:19:12 +0300

    Changes

---
+++ index.js
@@ -0,0 +1,14 @@
+const divide = (num1, num2) => {
+    return num1 / num2
+};
+divide(2000, 100);
+
+const square = (num) => {
+    return num * num;
+};
+square(20);
+
+const add = (num3, num4) => {
+    return num3 + num4;
+};
+add(65, 87);
